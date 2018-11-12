cd567968bc778d83d4f8b8e2b2af064f8c9bcae5
Tomasz Supłat <tomasz.suplat@student.put.poznan.pl>
2018-11-12 16:21:46 +0100

    test show of data in tfrecords

--- GQN.py
+++ GQN.py
@@ -1,1 +1,13 @@
-print('test123')
+import tensorflow as tf
+from data_reader import DataReader
+import matplotlib.pyplot as plt
+
+root_path = 'data'
+data_reader = DataReader(dataset='jaco', context_size=5, root=root_path)
+data = data_reader.read(batch_size=12)
+
+with tf.train.SingularMonitoredSession() as sess:
+    d = sess.run(data)
+    for img in d.target:
+        plt.imshow(img)
+        plt.show()
