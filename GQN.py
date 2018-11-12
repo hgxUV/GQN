e0380cca56d9b1bd2776f3ac75eb2346a58c102c
Tomasz Supłat <tomasz.suplat@student.put.poznan.pl>
2018-11-12 17:27:17 +0100

    added small template for convolution block

--- GQN.py
+++ GQN.py
@@ -2,6 +2,12 @@ import tensorflow as tf
 from data_reader import DataReader
 import matplotlib.pyplot as plt
 
+
+def conv_block(prev, size, k: tuple, s: tuple):
+    size_policy = 'same' if s == (1, 1) else 'valid'
+    return tf.layers.conv2d(prev, size, k, s, size_policy, activation=tf.nn.relu)
+
+
 root_path = 'data'
 data_reader = DataReader(dataset='jaco', context_size=5, root=root_path)
 data = data_reader.read(batch_size=12)
