cf05328d632bea50361cf0c77addcc356f21482d
Tomasz Supłat <tomasz.suplat@student.put.poznan.pl>
2018-11-15 18:11:50 +0100

    generative prototype (tower architecture)

--- GQN.py
+++ GQN.py
@@ -12,8 +12,33 @@ root_path = 'data'
 data_reader = DataReader(dataset='jaco', context_size=5, root=root_path)
 data = data_reader.read(batch_size=1)
 
-test = conv_block(data[1], 256, (2, 2), (2, 2))
+
+def generative_pipeline_tower(x, v):
+    input_img = x
+    input_v = tf.broadcast_to(v, (1, 16, 16, 7))
+
+    test = conv_block(input_img, 256, (2, 2), (2, 2))
+
+    # first residual
+    test2 = conv_block(test, 128, (3, 3), (1, 1))
+    test3 = tf.concat([test, test2], 3)
+    test4 = conv_block(test3, 256, (2, 2), (2, 2))
+
+    # add v
+    test5 = tf.concat([test4, input_v], 3)
+
+    # second residual
+    test6 = conv_block(test5, 128, (3, 3), (1, 1))
+    test7 = tf.concat([test6, test5], 3)
+    test8 = conv_block(test7, 256, (3, 3), (1, 1))
+
+    # last conv
+    return conv_block(test8, 256, (1, 1), (1, 1))
+
+
+xd = generative_pipeline_tower(data[1], data[0][1])
 
 with tf.train.SingularMonitoredSession() as sess:
-    d = sess.run(test)
+    d = sess.run(xd)
+    print(data[0])
 
