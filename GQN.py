c7a6e0cfd5759f0a5b2b16cffd9d6ce868851778
Tomasz Pecyna <tomasz.pecyna@tomtom.com>
2018-11-22 17:43:36 +0100

    fix sampling, ready for LSTM implementation

--- GQN.py
+++ GQN.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 def conv_block(prev, size, k: tuple, s: tuple):
     size_policy = 'same' if s == (1, 1) else 'valid'
-    return tf.layers.conv2d(prev, size, k, s, size_policy, activation=tf.nn.relu)
+    return tf.layers.conv2d(prev, size, k, s, size_policy)
 
 
 def representation_pipeline_tower(x, v):
@@ -34,16 +34,15 @@ def representation_pipeline_tower(x, v):
 def add_to_r(r, new_part):
     return tf.math.add(r, new_part)
 
-
-
 def sample_gaussian(mu, sigma=1.):
     sampled = tf.random_normal((), mean=0., stddev=1.)
     return tf.multiply(tf.math.add(mu, sampled), sigma)
 
-def prior_posterior(h_i):
-    gaussianStats = conv_block(h_i, 2, (5, 5), (1, 1))
-    means = gaussianStats[:, :, :, 0]
-    stds = gaussianStats[:, :, :, 1]
+def prior_posterior(h_i, n_features):
+    gaussianStats = conv_block(h_i, 2*n_features, (5, 5), (1, 1))
+    means = gaussianStats[:, :, :, 0:n_features]
+    stds = gaussianStats[:, :, :, n_features:]
+    stds = tf.nn.softmax(stds)
     gaussianStats = (means, stds)
     latent = tf.map_fn(lambda stats : sample_gaussian(stats[0], stats[1]), gaussianStats, dtype=tf.float32)
     return latent
@@ -58,7 +57,7 @@ def loss():
     return recon_loss + regularization_loss
 
 def observation_sample(u_L):
-    means = conv_block(u_L, 1, (1, 1), (1, 1))
+    means = conv_block(u_L, 3, (1, 1), (1, 1))
     x = tf.map_fn(lambda mean : sample_gaussian(mean), means, dtype=tf.float32)
     return x
 
@@ -68,17 +67,21 @@ def image_reconstruction(sampled):
     #x = tf.layers.conv2d_transpose(sampled, 128, 3, 16, 'SAME')
 
 
-
 root_path = 'data'
 data_reader = DataReader(dataset='rooms_ring_camera', context_size=5, root=root_path)
 data = data_reader.read(batch_size=1)
 print(data[1])
 
-xd = representation_pipeline_tower(data[1], data[0][1])
-someTensor = tf.random_normal([1, 16, 16, 256], 0, 1)
-test = prior_posterior(someTensor)
+#xd = representation_pipeline_tower(data[1], data[0][1])
+#someTensor = tf.random_normal([1, 16, 16, 256], 0, 1)
+#test = prior_posterior(someTensor, someTensor.shape[-1])
+u_L = tf.random_normal([1, 64, 64, 256], 0, 1)
+output_images = observation_sample(u_L)
+output_images = tf.clip_by_value(output_images, 0, 1)
 
 with tf.train.SingularMonitoredSession() as sess:
-    d = sess.run(test)
+    d = sess.run(output_images)
+    plt.imshow(d[0, :, :, :])
+    plt.show()
 
 a = 1
