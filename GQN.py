ae9c1b7064067d25f8f557c36142352ff6175415
Tomasz Pecyna <tomasz.pecyna@tomtom.com>
2018-12-04 22:33:42 +0100

    fixes, everything except tensorarrays with while loop seem to work

--- GQN.py
+++ GQN.py
@@ -9,8 +9,10 @@ n_reg_features = 256
 EPOCHS = 10
 
 
-gen_cell = tf.contrib.rnn.Conv2DLSTMCell(input_shape=[16, 16, 266], output_channels=256, kernel_shape=[5, 5], name='gen_cell')  # TODO: output channels, skip connection?
-inf_cell = tf.contrib.rnn.Conv2DLSTMCell(input_shape=[64, 64, 1066], output_channels=256, kernel_shape=[5, 5], name='inf_cell')  # TODO: output channels, skip connection?
+#TODO ask someone qualified about this global thingies
+gen_cell = tf.contrib.rnn.Conv2DLSTMCell(input_shape=[16, 16, 519], output_channels=256, kernel_shape=[5, 5], name='gen_cell')  # TODO: output channels, skip connection?
+inf_cell = tf.contrib.rnn.Conv2DLSTMCell(input_shape=[16, 16, 775], output_channels=256, kernel_shape=[5, 5], name='inf_cell')  # TODO: output channels, skip connection?
+training = False
 
 
 def conv_block(prev, size, k: tuple, s: tuple):
@@ -71,6 +73,7 @@ def prior_posterior(h_i):
     means = gaussianParams[:, :, :, 0:n_reg_features]
     stds = gaussianParams[:, :, :, n_reg_features:]
     stds = tf.nn.softmax(stds)
+    gaussianParams = tf.concat([means, stds], -1)
     distributions = tf.distributions.Normal(loc=means, scale=stds)
     latent = distributions.sample()
     return (latent, gaussianParams)
@@ -84,14 +87,22 @@ def regularization_loss(prior, posterior):
     posterior = posterior.concat()
     distritution = lambda x : tf.distributions.Normal(loc=x[:, :, :, 0:n_reg_features],
                                                       scale=x[:, :, :, n_reg_features:])
+    '''prior_l = prior[:, :, :, 0:n_reg_features]
+    prior_s = prior[:, :, :, n_reg_features:]
+    posterior_l = posterior[:, :, :, 0:n_reg_features]
+    posterior_s = posterior[:, :, :, n_reg_features:]
+    with tf.train.SingularMonitoredSession() as sess:
+        a, b, c, d = sess.run([prior_l, prior_s, posterior_l,posterior_s])'''
     prior = distritution(prior)
     posterior = distritution(posterior)
     reg_loss = tf.distributions.kl_divergence(posterior, prior)
-    reg_loss = tf.reduce_sum(reg_loss)
+    #with tf.train.SingularMonitoredSession() as sess:
+    #    a = sess.run(reg_loss)
+    reg_loss = tf.reduce_mean(reg_loss)
     return reg_loss
 
 def calculate_loss(priors, posteriors, x_pred, x_q):
-    return recon_loss(x_q, x_pred) + regularization_loss(priors, posteriors)
+    return tf.add(recon_loss(x_q, x_pred), regularization_loss(priors, posteriors))
 
 def observation_sample(u_L):
     means = conv_block(u_L, 3, (1, 1), (1, 1))
@@ -106,20 +117,18 @@ def image_reconstruction(u):
     return x_pred
 
 
-def body(state_g, u, r, v_q, x_q, state_i, priors, posteriors, i, training):
+def body(state_g, u, r, v_q, x_q, state_i, priors, posteriors, i):
 
     #inference
     if(training):
-        #TODO make x_q 16x16 somehow, mby tower representation is a good option
         concat_i = tf.concat([x_q,
                               tf.broadcast_to(v_q, (v_q.shape[0], 16, 16, v_q.shape[-1])),
                               r,
-                              state_g.h,
-                              ], 3)
+                              state_g.h],
+                             3)
         h_i, state_i = inf_cell(concat_i, state_i)
 
-        posterior_input = tf.layers.max_pooling2d(h_i, (4, 4), (4, 4), padding='SAME')
-        posterior_latent, posterior_params = prior_posterior(posterior_input)
+        posterior_latent, posterior_params = prior_posterior(state_i.h)
         posteriors = posteriors.write(i, posterior_params)
 
     #generation
@@ -131,21 +140,23 @@ def body(state_g, u, r, v_q, x_q, state_i, priors, posteriors, i, training):
 
     concat_g = tf.concat([v_q,
                           r,
-                          u,
                           prior_latent], 3)
     h_g, state_g = gen_cell(concat_g, state_g)
     u = tf.math.add(tf.layers.conv2d_transpose(h_g, 256, 4, 4, 'SAME'), u)
 
     i += 1
 
-    return state_g, u, r, v_q, x_q, state_i, priors, posteriors, i, training
+    return state_g, u, r, v_q, x_q, state_i, priors, posteriors, i
 
-def cond(state_g, u, r, v_q, x_q, state_i, priors, posteriors, i, training):
+def cond(state_g, u, r, v_q, x_q, state_i, priors, posteriors, i):
     dec = tf.less(L, i)
     return dec
 
 
-def architecture(x, v, v_q, x_q, training):
+def architecture(x, v, v_q, x_q, training_local):
+    global training
+    training = training_local
+    x_gt = x_q
     state_g = gen_cell.zero_state(BATCH_SIZE, dtype='float32') # c + h
     u = tf.zeros([x.shape[0], 64, 64, 256])
 
@@ -154,19 +165,26 @@ def architecture(x, v, v_q, x_q, training):
     state_i = inf_cell.zero_state(BATCH_SIZE, dtype='float32') # c + h
 
     i = 0
-    priors = tf.TensorArray(dtype=tf.float32, size=12, element_shape=[x.shape[0], 16, 16, n_reg_features*2])
-    posteriors = tf.TensorArray(dtype=tf.float32, size=12, element_shape=[x.shape[0], 16, 16, n_reg_features*2])
+    priors = tf.TensorArray(dtype=tf.float32, size=12, element_shape=[x.shape[0], 16, 16, n_reg_features*2])#,  clear_after_read=False)
+    posteriors = tf.TensorArray(dtype=tf.float32, size=12, element_shape=[x.shape[0], 16, 16, n_reg_features*2])#,  clear_after_read=False)
 
     v_q = tf.broadcast_to(tf.expand_dims(tf.expand_dims(v_q, 1), 1), (x.shape[0], 16, 16, 7))
+    #TODO make x_q 16x16 somehow, mby tower representation is a good option
+    x_q = tf.zeros([x.shape[0], 16, 16, 256])
 
-    variables = (state_g, u, r, v_q, x_q, state_i, priors, posteriors, i, training)
+    variables = (state_g, u, r, v_q, x_q, state_i, priors, posteriors, i)
 
-    variables = tf.while_loop(cond, body, variables)
-    state_g, u, r, v_q, x_q, state_i, priors, posteriors, i, training = variables
+    variables = tf.while_loop(cond, body, variables, parallel_iterations=1)
+    #variables = body(*variables)
+    #variables = body(*variables)
+    #variables = body(*variables)
+
+    state_g, u, r, v_q, x_q, state_i, priors, posteriors, i = variables
 
     x_pred = image_reconstruction(u)
-    #TODO reconstruction loss, distribution loss
-    loss = calculate_loss(priors, posteriors, x_pred, x_q)
+    loss = calculate_loss(priors, posteriors, x_pred, x_gt)
+
+    tf.TensorArray.write()
 
     return x_pred, loss
 
@@ -192,16 +210,16 @@ v_q = data.query.query_camera
 #v = tf.placeholder(tf.float32, [BATCH_SIZE, 5, 7], 'v')
 #v_q = tf.placeholder(tf.float32, [BATCH_SIZE, 7], 'v_q')
 #x_q = tf.placeholder(tf.float32, [BATCH_SIZE, 64, 64, 3], 'x_q')
-image, loss = architecture(x, v, v_q, x_q, training=True)
+image, loss = architecture(x, v, v_q, x_q, training_local=True)
 train_op = optimizer.minimize(loss)
 
 with tf.train.SingularMonitoredSession() as sess:
     for i in range(EPOCHS):
         for _ in range(4):
             #image, train_op = sess.run([image, train_op])#, feed_dict={x: x, v: v, v_q: v_q, x_q: x_q})
-            x_pred, loss = sess.run([image, train_op])  # , feed_dict={x: x, v: v, v_q: v_q, x_q: x_q})
-            print(loss)
-            plt.imshow(x_pred[0, 0, ...])
+            x_pred, loss_value = sess.run([image, loss])  # , feed_dict={x: x, v: v, v_q: v_q, x_q: x_q})
+            print(loss_value)
+            plt.imshow(x_pred[0, ...])
             plt.show()
 
 a = 1
