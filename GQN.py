aaa296caeab52d87e2d10d1860c3466f306ae197
Tomasz Supłat <wuwrut@gmail.com>
2018-12-08 18:59:13 +0100

    cleanup, added 2 variable scopes

--- GQN.py
+++ GQN.py
@@ -1,6 +1,5 @@
 import tensorflow as tf
 from data_reader import DataReader
-import matplotlib.pyplot as plt
 import datetime
 import os
 
@@ -26,9 +25,8 @@ def conv_block(prev, size, k: tuple, s: tuple):
 
 
 # x shape: (1, 64, 64, 3), v shape: (1, 7)
-def representation_pipeline_tower(x, v=None, representation=True):
-    if(representation):
-        v = tf.broadcast_to(tf.expand_dims(tf.expand_dims(v, 1), 1), (x.shape[0], 16, 16, 7))
+def representation_pipeline_tower(x, v):
+    v = tf.broadcast_to(tf.expand_dims(tf.expand_dims(v, 1), 1), (x.shape[0], 16, 16, 7))
 
     x = conv_block(x, 256, (2, 2), (2, 2))
 
@@ -38,8 +36,7 @@ def representation_pipeline_tower(x, v=None, representation=True):
     x = conv_block(x, 256, (2, 2), (2, 2))
 
     # add v
-    if(representation):
-        x = tf.concat([x, v], 3)
+    x = tf.concat([x, v], 3)
 
     # second residual
     y = conv_block(x, 128, (3, 3), (1, 1))
@@ -52,10 +49,6 @@ def representation_pipeline_tower(x, v=None, representation=True):
 
 # frames shape: (5, 64, 64, 3), v shape: (5, 7)
 def create_representation(frames, cameras):
-    #assert frames.shape[0] == cameras.shape[0]
-    #assert len(frames.shape) == 4
-    #assert len(cameras.shape) == 2
-
     frames = tf.reshape(frames, (-1, 64, 64, 3))
     cameras = tf.reshape(cameras, (-1, 7))
 
@@ -67,10 +60,6 @@ def create_representation(frames, cameras):
     return r
 
 
-def sample_gaussian(mu, sigma=1.):
-    sampled = tf.random_normal((), mean=0., stddev=1.)
-    return tf.multiply(tf.math.add(mu, sampled), sigma)
-
 def prior_posterior(h_i):
     gaussianParams = conv_block(h_i, 2*n_reg_features, (5, 5), (1, 1))
     means = gaussianParams[:, :, :, 0:n_reg_features]
@@ -79,12 +68,14 @@ def prior_posterior(h_i):
     gaussianParams = tf.concat([means, stds], -1)
     distributions = tf.distributions.Normal(loc=means, scale=stds)
     latent = distributions.sample()
-    return (latent, gaussianParams)
+    return latent, gaussianParams
+
 
 def recon_loss(x_true, x_pred):
     rec_loss = tf.losses.mean_squared_error(x_true, x_pred)
     tf.summary.scalar('reconstruction_loss', rec_loss)
     return rec_loss
+
 
 def distribution_loss(prior, posterior):
     prior = prior.concat()
@@ -98,10 +89,12 @@ def distribution_loss(prior, posterior):
     tf.summary.scalar('distribution_loss', dist_loss)
     return dist_loss
 
+
 def calculate_loss(priors, posteriors, x_pred, x_q):
     loss = tf.add(recon_loss(x_q, x_pred), distribution_loss(priors, posteriors))
     tf.summary.scalar('loss', loss)
     return loss
+
 
 def image_reconstruction(u):
     x = conv_block(u, 3, (1, 1), (1, 1))
@@ -112,35 +105,33 @@ def image_reconstruction(u):
 
 
 def body(state_g, u, r, v_q, x_q, state_i, priors, posteriors, i):
+    with tf.variable_scope('inference'):
+        if(training):
+            concat_i = tf.concat([x_q,
+                                  v_q,
+                                  r,
+                                  state_g.h],
+                                 3)
+            h_i, state_i = inf_cell(concat_i, state_i)
 
-    #inference
-    if(training):
-        concat_i = tf.concat([x_q,
-                              v_q,
+            posterior_latent, posterior_params = prior_posterior(state_i.h)
+            posteriors = posteriors.write(i, posterior_params)
+
+    with tf.variable_scope('generation'):
+        prior_latent, prior_params = prior_posterior(state_g.h)
+        priors = priors.write(i, prior_params)
+
+        if(training):
+            prior_latent = posterior_latent
+
+        concat_g = tf.concat([v_q,
                               r,
-                              state_g.h],
-                             3)
-        h_i, state_i = inf_cell(concat_i, state_i)
+                              prior_latent], 3)
+        h_g, state_g = gen_cell(concat_g, state_g)
+        u = tf.math.add(tf.layers.conv2d_transpose(h_g, 256, 4, 4, 'SAME'), u)
 
-        posterior_latent, posterior_params = prior_posterior(state_i.h)
-        posteriors = posteriors.write(i, posterior_params)
+    return state_g, u, r, v_q, x_q, state_i, priors, posteriors, i + 1
 
-    #generation
-    prior_latent, prior_params = prior_posterior(state_g.h)
-    priors = priors.write(i, prior_params)
-
-    if(training):
-        prior_latent = posterior_latent
-
-    concat_g = tf.concat([v_q,
-                          r,
-                          prior_latent], 3)
-    h_g, state_g = gen_cell(concat_g, state_g)
-    u = tf.math.add(tf.layers.conv2d_transpose(h_g, 256, 4, 4, 'SAME'), u)
-
-    i += 1
-
-    return state_g, u, r, v_q, x_q, state_i, priors, posteriors, i
 
 def cond(state_g, u, r, v_q, x_q, state_i, priors, posteriors, i):
     dec = tf.less(i, L)
@@ -160,12 +151,12 @@ def generative_query_network(x, v, v_q, x_q, training_local):
     u = tf.zeros([x.shape[0], 64, 64, 256])
     r = create_representation(x, v)
     i = 0
-    #x_q = representation_pipeline_tower(x_q, representation=False)
+
     x_q = tf.image.resize_images(x_q, (16, 16))
     v_q = tf.broadcast_to(tf.expand_dims(tf.expand_dims(v_q, 1), 1), (x.shape[0], 16, 16, 7))
 
-    priors = tf.TensorArray(dtype=tf.float32, size=12, element_shape=[x.shape[0], 16, 16, n_reg_features*2])#,  clear_after_read=False)
-    posteriors = tf.TensorArray(dtype=tf.float32, size=12, element_shape=[x.shape[0], 16, 16, n_reg_features*2])#,  clear_after_read=False)
+    priors = tf.TensorArray(dtype=tf.float32, size=12, element_shape=[x.shape[0], 16, 16, n_reg_features*2])
+    posteriors = tf.TensorArray(dtype=tf.float32, size=12, element_shape=[x.shape[0], 16, 16, n_reg_features*2])
 
     variables = (state_g, u, r, v_q, x_q, state_i, priors, posteriors, i)
 
@@ -210,13 +201,10 @@ with tf.train.SingularMonitoredSession() as sess:
         for j in range(int((TF_RECORDS * RECORDS_IN_TF_RECORD) / BATCH_SIZE)):
             x_pred, x_gt, loss_value, summary = sess.run([train_output, x_q, train_loss, merged_summaries])  # , feed_dict={x: x, v: v, v_q: v_q, x_q: x_q})
             train_writer.add_summary(summary, j)
-            #x_gt = sess.run(x_q)
+
             total_loss.append(loss_value)
             print(loss_value)
-            #plt.imshow(x_gt[0, ...])
-            #plt.show()
-            #plt.imshow(x_pred[0, ...])
-            #plt.show()
+
             j += 1
             print(j)
         print(j)
